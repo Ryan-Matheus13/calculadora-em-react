3c7f0d8b00240a3df477e23a34a041426c9a0cac
Ryan Matheus <ryanmatheusdev@gmail.com>
2022-02-23 20:48:37 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Button from './components/Button';
+
+//sfc
+const App = () => {
+  return ( 
+    <>
+      <div className='container'>
+        <Button></Button>
+      </div>
+    </>
+   );
+}
+ 
+export default App;
